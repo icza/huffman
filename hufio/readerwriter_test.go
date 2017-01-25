3d0826129c717ad11e2aeacf22f4f8d6a6d73f69
Andras Belicza <iczaaa@gmail.com>
2017-01-25 14:06:17 +0100

    Extended tests to cover error branches. Reached 100% coverage.

--- hufio/readerwriter_test.go
+++ hufio/readerwriter_test.go
@@ -2,7 +2,9 @@ package hufio
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"testing"
@@ -103,5 +105,78 @@ func testWriteAndRead(testCase string, data []byte, t *testing.T, os ...*Options
 	}
 	if !bytes.Equal(data, data2) {
 		t.Error("Decoded doesn't match original!", len(data2))
+	}
+}
+
+type errReader struct {
+	data []byte // Data to simulate before reporting error
+}
+
+func (r *errReader) Read(p []byte) (n int, err error) {
+	if len(r.data) > 0 {
+		n := copy(p, r.data)
+		r.data = r.data[n:]
+		return n, nil
+	}
+	return 0, errors.New("Intentional testing error")
+}
+
+func TestReaderError(t *testing.T) {
+	cases := []struct {
+		data []byte
+	}{
+		{nil},          // Cover br.ReadBool() call in ReadByte()
+		{[]byte{0x80}}, // Cover br.ReadByte() call in ReadByte()
+	}
+
+	for _, c := range cases {
+		r := NewReader(&errReader{c.data})
+		if _, err := io.ReadFull(r, make([]byte, len(c.data)+1)); err == nil {
+			t.Error("Expected error but succeeded.")
+		}
+	}
+}
+
+type errWriter struct {
+	bitsErr, bytesErr error
+}
+
+func (w *errWriter) WriteBool(b bool) (err error)           { return }
+func (w *errWriter) Align() (skipped byte, err error)       { return }
+func (w *errWriter) Write(p []byte) (n int, err error)      { return }
+func (w *errWriter) Close() (err error)                     { return }
+func (w *errWriter) WriteByte(b byte) (err error)           { return w.bytesErr }
+func (w *errWriter) WriteBits(r uint64, n byte) (err error) { return w.bitsErr }
+
+func TestWriterError(t *testing.T) {
+	w := NewWriter(nil)
+
+	// "Put" 'b' into the writer (so it won't be a new symbol):
+	w.(*writer).bw = &errWriter{}
+	if err := w.WriteByte('b'); err != nil {
+		panic(err)
+	}
+
+	te := errors.New("Intetional testing error")
+
+	cases := []struct {
+		bitsErr, bytesErr error
+		symbol            byte
+	}{
+		{te, nil, 'a'}, // Cover 1st w.bw.WriteBits() in WriteByte
+		{nil, te, 'a'}, // Cover w.bw.WriteByte() in WriteByte
+		{te, nil, 'b'}, // Cover 2nd w.bw.WriteBits() in WriteByte
+	}
+
+	for _, c := range cases {
+		w.(*writer).bw = &errWriter{c.bitsErr, c.bytesErr}
+		if _, err := w.Write([]byte{c.symbol}); err == nil {
+			t.Error("Expected error but succeeded.")
+		}
+	}
+
+	w.(*writer).bw = &errWriter{bitsErr: te}
+	if err := w.Close(); err == nil {
+		t.Error("Expected error but succeeded.")
 	}
 }
